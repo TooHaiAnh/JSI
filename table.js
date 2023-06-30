fec1c427cd9cf45e2af0ca16dcdd1797c2e89cdd
dnhd0181 <118668638+dnhd0181@users.noreply.github.com>
2023-06-30 18:42:05 +0700

    Add files via upload

---
+++ table.js
@@ -0,0 +1,104 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js"
+import { getFirestore, collection, getDoc, addDoc, doc, onSnapshot,setDoc,updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js"
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = { 
+    apiKey : "AIzaSyC-05yE959HrF8NNfgkKkuAAHypTXncn5g" , 
+    authDomain : "baicuoikhoajsi.firebaseapp.com" , 
+    projectId : "baicuoikhoajsi" , 
+    storageBucket : "baicuoikhoajsi.appspot.com" , 
+    messagingSenderId : "953335025294" , 
+    appId : "1:953335025294:web:b62d51edc0dd60d37183df" , 
+    measurementId : "G-8JVVLVMVYX" 
+    
+  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+const db = getFirestore(app);
+
+let NameBox =document.getElementById("Namebox");
+let Link =document.getElementById("Link")
+let Rollbox =document.getElementById("Rollbox");
+
+let AddBtn =document.getElementById("Addbtn")
+let UpdBtn =document.getElementById("Updbtn")
+let DelBtn =document.getElementById("Delbtn")
+let RetBtn =document.getElementById("Retbtn")
+
+
+
+async function AddDocument_CustomId() {
+    var ref = doc(db,"information",Rollbox.value);
+
+    const docRef = await setDoc(
+        ref,{
+            Name : NameBox.value,
+            RollNo:Rollbox.value,
+            Link:Link.value,
+
+        }
+    )
+    .then(()=> {alert( "data added");})
+    .catch((error)=> {alert( "data"+error);});
+}
+AddBtn.addEventListener("click",AddDocument_CustomId)
+
+
+
+
+async function GetData(){
+    var ref = doc(db,"information",Rollbox.value);
+    const  docSnap = await getDoc(ref);
+    if(docSnap.exists()) {
+        NameBox.value = docSnap.data().Name;
+        Link.value = docSnap.data().Link;
+}
+
+    else{
+        alert("data not found");
+    }}
+RetBtn.addEventListener("click",GetData)
+
+
+
+
+
+
+
+async function UpdateData() {
+    var ref = doc(db,"information",Rollbox.value);
+
+ updateDoc(
+        ref,{
+            Name : NameBox.value,
+            Link:Link.value,
+
+        }
+    )
+    .then(()=> {alert( "data updated");})
+    .catch((error)=> {alert( "data "+error);});
+}
+UpdBtn.addEventListener("click",UpdateData)
+
+
+
+
+
+async function DeleteData(){
+    var ref = doc(db,"information",Rollbox.value);
+    const  docSnap = await getDoc(ref);
+    
+    
+    
+    if(!docSnap.exists()) {
+       alert("data does not exist");
+       return;
+}
+    await deleteDoc(ref)
+    .then(()=> {alert( "data deleted");})
+    .catch((error)=> {alert( "data not deleted " +error);});
+     }
+DelBtn.addEventListener("click",DeleteData) 
