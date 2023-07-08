8f979e0fdb49357e2ec54393fde3c017955006ef
dnhd0181 <118668638+dnhd0181@users.noreply.github.com>
2023-07-08 21:02:04 +0700

    Add files via upload

---
+++ newDiv.js
@@ -0,0 +1,3 @@
+console.log("Description ", JSON.parse(localStorage.getItem("Description")))
+console.log("Name ", JSON.parse(localStorage.getItem("Name")))
+console.log("Píc ", JSON.parse(localStorage.getItem("Pic")))
