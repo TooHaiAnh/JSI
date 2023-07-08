5e7dd678a184dd51036c393b8166c14f550fde8a
dnhd0181 <118668638+dnhd0181@users.noreply.github.com>
2023-07-08 21:16:33 +0700

    Update newDiv.js

--- newDiv.js
+++ newDiv.js
@@ -1,3 +1,9 @@
 console.log("Description ", JSON.parse(localStorage.getItem("Description")))
 console.log("Name ", JSON.parse(localStorage.getItem("Name")))
 console.log("Píc ", JSON.parse(localStorage.getItem("Pic")))
+document.getElementById("Name").innerHTML = JSON.parse(localStorage.getItem("Name"))
+document.getElementById("Description").innerHTML = JSON.parse(localStorage.getItem("Description"))
+document.getElementById("loadimg").onclick = () =>{
+            let img = document.createElement('img');
+            img.src = JSON.parse(localStorage.getItem("Pic"))
+            document.getElementById('body').appendChild(img);}
